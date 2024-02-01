f6f5fc7687c5da34372c93069bb4c0c375d27b99
emon02 <abdulalimemon11@gmail.com>
2024-02-01 16:05:41 +0600

    hero section complete

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,11 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
     <header className="h-16">
-      <nav className="w-full max-w-[1220px] mx-auto flex justify-between items-center h-full px-[20px]">
-        <span className="text-3xl">iReapir</span>
+      <nav className="w-full max-w-[1230px] mx-auto flex justify-between items-center h-full px-[20px]">
+        <Link to="/">
+          <span className="text-3xl">iReapir</span>
+        </Link>
+
         <ul className=" space-x-5 font-semibold">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -1,22 +1,31 @@
 import Container from "@/components/layouts/Container";
 import { Button } from "@/components/ui/button";
+import mackbook from "@/assets/image/macbook-exposed.png";
 
 const HeroSection = () => {
   return (
-    <Container>
-      <div>
-        <h1>
-          <span>Don't worry.</span>
+    <Container className="grid grid-cols-1 lg:grid-cols-2 h-[calc(100vh-64px)] place-content-center">
+      <div className="mt-20 lg:mt-0 w-full mx-auto">
+        <h1 className="text-5xl lg:text-8xl font-bold text-nowrap">
+          <span className="text-gray">Don't worry.</span>
           <br />
           <span>We'll fix it.</span>
         </h1>
-        <p>
-          Welcome to <span>iRepair</span>, your one-stop place for all kinds of{" "}
-          <span>Macbook repairs</span> and diagnostics.
+        <p className="text-xl text-gray mt-10 mb-5 max-w-[50ch]">
+          Welcome to <span className="text-black font-semibold">iRepair</span>,
+          your one-stop place for all kinds of{" "}
+          <span className="text-black font-semibold">Macbook repairs</span> and
+          diagnostics.
         </p>
         <Button>Book a service</Button>
       </div>
-      <div></div>
+      <div className="mt-10 lg:mt-0 w-3/4 lg:w-full mx-auto">
+        <img
+          className="-rotate-[30deg] h-[95%] object-contain"
+          src={mackbook}
+          alt="MackBook"
+        />
+      </div>
     </Container>
   );
 };
